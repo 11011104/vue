cada7e3d8e77a2ae17b67e73f7b820550b08e861
mm <1305354640@qq.com>
2020-01-02 20:24:08 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/css/Global.css'
 /*导入字体图标 */
 import "./assets/fonts/iconfont.css"
 import axios from 'axios'
+//请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config=>{
+  //为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  return config
+})
 axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
 
 Vue.prototype.$http=axios
